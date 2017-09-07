b89f6d368fdf6a9431a01691d5c329839015d69f
Niko Kristian Pinnis <niko.pinnis@gmail.com>
2017-09-08 00:55:18 +0300

    Add todosReducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,6 +1,8 @@
 const expect = require('expect');
 const reducers = require('reducers');
-var df = require('deep-freeze-strict');
+const df = require('deep-freeze-strict');
+const uuid = require('node-uuid');
+const moment = require('moment');
 
 describe('Reducers', () => {
   describe('searchTextReducer', () => {
@@ -22,6 +24,36 @@ describe('Reducers', () => {
 
       var res = reducers.showCompletedReducer(df(false), df(action));
       expect(res).toEqual(true);
+    });
+  });
+  describe('todosReducer', () => {
+    it('should add a new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Test a new todo'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+    it('should toggle the completed status of a todo', () => {
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: uuid()
+      };
+      var todos = [
+        {
+          id: action.id,
+          text: 'test todo',
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
+      ];
+      var res = reducers.todosReducer(df(todos), df(action));
+      expect(res[0].completed).toEqual(true);
+      expect(res[0].completedAt).toEqual(moment().unix());
     });
   });
 });
