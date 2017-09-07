5462f28053178d0a7e5f7b23cb660061ac992fe7
Niko Kristian Pinnis <niko.pinnis@gmail.com>
2017-09-08 00:10:48 +0300

    Add showCompleted and searchText reducers and deep-freeze support

---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,27 @@
+const expect = require('expect');
+const reducers = require('reducers');
+var df = require('deep-freeze-strict');
+
+describe('Reducers', () => {
+  describe('searchTextReducer', () => {
+    it('should set searchText', () => {
+      var action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'test'
+      };
+      var res = reducers.searchTextReducer(df(''), df(action));
+
+      expect(res).toEqual(action.searchText);
+    });
+  });
+  describe('showCompletedReducer', () => {
+    it('should flip the status of showCompleted', () => {
+      var action = {
+        type: 'TOGGLE_SHOW_COMPLETED'
+      };
+
+      var res = reducers.showCompletedReducer(df(false), df(action));
+      expect(res).toEqual(true);
+    });
+  });
+});
