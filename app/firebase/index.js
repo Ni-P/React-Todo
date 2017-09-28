f9214feec5a210b5c05579ba953e978b90e0c395
Niko Kristian Pinnis <niko.pinnis@gmail.com>
2017-09-28 06:25:29 +0300

    Set .env files for test & prod API and delete old public db & API key

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: 'AIzaSyA9VR0tHY-4ufvkFBrPGcsWWD_8zmEEviA',
-    authDomain: 'nip-react-todo-app.firebaseapp.com',
-    databaseURL: 'https://nip-react-todo-app.firebaseio.com',
-    projectId: 'nip-react-todo-app',
-    storageBucket: 'nip-react-todo-app.appspot.com',
-    messagingSenderId: '102391914318'
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    // projectId: 'nip-react-todo-app',
+    storageBucket: process.env.STORAGE_BUCKET
+    // messagingSenderId: '102391914318'
   };
   firebase.initializeApp(config);
 } catch (e) {}
